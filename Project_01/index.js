5bc2160d571224423d75ca7f8f297110ecff6323
Tejas <tejasambore2003@gmail.com>
2025-04-23 14:18:50 +0530

    HTTP Status Code

--- Project_01/index.js
+++ Project_01/index.js
@@ -45,9 +45,11 @@ app.get('/api/users', (req, res) => {
 //     return res.json(user);
 // })
 
-app.route('/api/users/:id').get((req, res) => {
+app.route('/api/users/:id')
+    .get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
+        if (!user) return res.status(404).json({ error: "user not found.."})
         return res.json(user);
     })
     .patch((req, res) => {
@@ -63,9 +65,19 @@ app.post('/api/users', (req, res) => {
     // TODO: Create new user
     const body = req.body;
     // console.log("Body", body)
+    if (
+        !body || 
+        !body.first_name || 
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ) {
+        return res.status(400).json({ error: "Please provide all the fields" });
+    }
     users.push({ id: users.length + 1, ...body});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json({ status: "pending", id: users.length });
+        return res.status(201).json({ status: "success", id: users.length });
     })
 })
 
